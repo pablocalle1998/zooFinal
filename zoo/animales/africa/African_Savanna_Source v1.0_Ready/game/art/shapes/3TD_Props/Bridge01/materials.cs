80a9cfc621c4661c61ba0e79070ea05bdb6bad71
pablocalle1998 <34372799+pablocalle1998@users.noreply.github.com>
2019-08-05 15:48:33 -0500

    primero

---
+++ zoo/animales/africa/African_Savanna_Source v1.0_Ready/game/art/shapes/3TD_Props/Bridge01/materials.cs
@@ -0,0 +1,16 @@
+
+singleton Material(Bridge01_ColorEffectR184G155B229_material)
+{
+   mapTo = "ColorEffectR184G155B229-material";
+   diffuseColor[0] = "0.721569 0.607843 0.898039 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(Bridge01_ColorEffectR177G28B149_material)
+{
+   mapTo = "ColorEffectR177G28B149-material";
+   diffuseColor[0] = "0.694118 0.109804 0.584314 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
